b3e622f84b38a2db348663f909777beb18e0640f
cjhaddad & Shannon Staszak <pair+cjhaddad.fancyultrafresh@devbootcamp.com>
2015-05-30 20:38:35 -0500

    iconicmodal connected to controller and modal temp created

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,5 @@
 
-app.controller('ActiveDecisionCtrl', ['$scope', 'Decision', function($scope, Decision){
+app.controller('ActiveDecisionCtrl', ['$scope', 'Decision', '$ionicModal', function($scope, Decision, $ionicModal){
 
     $scope.decisionUsers = Decision.query();
     $scope.decisionName = "Pizza Tonight!"
@@ -8,5 +8,39 @@ app.controller('ActiveDecisionCtrl', ['$scope', 'Decision', function($scope, Dec
     //   var query = new Query($scope.queryData);
     //   query.$save();
     // }
-}]);
+    // $scope.newProposal = function(){
+    //   alert("hi!")
+    // }
+    $ionicModal.fromTemplateUrl('new_proposal.html', function(modal){
+      $scope.proposalModal = modal;
+    }, {
+      scope: $scope,
+      animation: 'slide-in-up'
+    });
+
+    $scope.newProposal = function(){
+      alert("hi!")
+    }
+
+    $scope.cancelNewProposal = function(){
+      $scope.proposalModal.hide();
+    }
+}])
+// .controller('modalCtrl', ['$scope', function($scope,  $ionicModal){
+
+//   $ionicModal.fromTemplateUrl('new_proposal.html', function(modal){
+//       $scope.proposalModal = modal;
+//     }, {
+//       scope: $scope,
+//       animation: 'slide-in-up'
+//     });
+
+//     $scope.newProposal = function(){
+//       alert("hi!")
+//     }
+
+//     $scope.cancelNewProposal = function(){
+//       $scope.proposalModal.hide();
+//     }
+// }]);
 
